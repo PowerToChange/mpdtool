07c3d37664b8dad13a4651fb763cf538d1f666ab
Andrew Roth <andrewroth@andrew-roths-macbook-pro.local>
2009-01-07 14:23:52 -0800

    removed file_column

--- app/models/mpd_letter_image.rb
+++ app/models/mpd_letter_image.rb
@@ -3,10 +3,6 @@ class MpdLetterImage < ActiveRecord::Base
                  :thumbnails => { :thumb => '50x50!', :print => '400>' }
 
   belongs_to :mpd_letter
-  file_column :image, :magick => {:versions => {:thumb  => {:crop => '1:1', :size => "50x50!"},
-                                                :print => {:geometry => "400>"}}
-  	                	          }
-  validates_file_format_of :image, :in => ["png", "jpg", 'JPG', 'PNG']
   
   def image_name
     str = self.image.split("/")
