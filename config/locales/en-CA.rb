610c96b8dcb566f675c23d4f3371a1d36239f7a4
Josh <machomagna@gmail.com>
2009-01-08 12:10:26 -0600

    add locale file for canada

---
+++ config/locales/en-CA.rb
@@ -0,0 +1,59 @@
+{
+  :'en-CA' => {
+    # Date and Time Formats
+    :date => {
+      :formats => {
+        :default      => "%d/%m/%Y",
+        :short        => "%e %b",
+        :long         => "%e %B, %Y",
+        :long_ordinal => lambda { |date| "#{date.day.ordinalize} %B, %Y" },
+        :short_ordinal => lambda { |date| "#{date.day.ordinalize} %B" },
+        :only_day     => "%e"
+      },
+      :day_names => Date::DAYNAMES,
+      :abbr_day_names => Date::ABBR_DAYNAMES,
+      :month_names => Date::MONTHNAMES,
+      :abbr_month_names => Date::ABBR_MONTHNAMES,
+      :order => [:year, :month, :day]
+    },
+    :time => {
+      :formats => {
+        :default      => "%a %b %d %H:%M:%S %Z %Y",
+        :time         => "%H:%M",
+        :short        => "%d %b %H:%M",
+        :long         => "%d %B, %Y %H:%M",
+        :long_ordinal => lambda { |time| "#{time.day.ordinalize} %B, %Y %H:%M" },
+        :short_ordinal => lambda { |time| "#{time.day.ordinalize} %B" },
+        :only_second  => "%S"
+      },
+      :datetime => {
+        :formats => {
+          :default => "%Y-%m-%dT%H:%M:%S%Z"
+        }
+      },
+      :time_with_zone => {
+        :formats => {
+          :default => lambda { |time| "%Y-%m-%d %H:%M:%S #{time.formatted_offset(false, 'UTC')}" }
+        }
+      },
+      :am => 'am',
+      :pm => 'pm'
+    },
+    :datetime => {
+      :distance_in_words => {
+        :half_a_minute       => 'half a minute',
+        :less_than_x_seconds => {:zero => 'less than a second', :one => 'less than a second', :other => 'less than {{count}} seconds'},
+        :x_seconds           => {:one => '1 second', :other => '{{count}} seconds'},
+        :less_than_x_minutes => {:zero => 'less than a minute', :one => 'less than a minute', :other => 'less than {{count}} minutes'},
+        :x_minutes           => {:one => "1 minute", :other => "{{count}} minutes"},
+        :about_x_hours       => {:one => 'about 1 hour', :other => 'about {{count}} hours'},
+        :x_days              => {:one => '1 day', :other => '{{count}} days'},
+        :about_x_months      => {:one => 'about 1 month', :other => 'about {{count}} months'},
+        :x_months            => {:one => '1 month', :other => '{{count}} months'},
+        :about_x_years       => {:one => 'about 1 year', :other => 'about {{count}} years'},
+        :over_x_years        => {:one => 'over 1 year', :other => 'over {{count}} years'}
+      }
+    },
+    :welcome_letter => 'Canada sucks'
+  }
+}
