0ea6a495261c9d59daee453699f531f788d44c2d
Josh <machomagna@gmail.com>
2009-01-12 14:18:15 -0600

    alternate filename for importing images

--- lib/import_images.rb
+++ lib/import_images.rb
@@ -22,6 +22,10 @@ for mpd_letter_image in MpdLetterImage.find(:all)
   image_filename = mpd_letter_image[:image]
   unless image_filename.blank?
     image_path = File.join(RAILS_ROOT, 'public', 'files', 'mpd_letter_image', 'image', '0000', sprintf('%04d', mpd_letter_image.id), image_filename)
+    # also try the other kind of path without padding
+    unless File.exists?(image_path)
+      image_path = File.join(RAILS_ROOT, 'public', 'files', 'mpd_letter_image', 'image', mpd_letter_image.id, image_filename)
+    end
     if File.exists?(image_path)
       # raise image_path
       image_file = File.open(image_path, 'r')
