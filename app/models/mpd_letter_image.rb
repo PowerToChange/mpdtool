13c61d2cc3d68477a183376baff9c9aa6f8e7b01
Josh Starcher <josh.starcher@gmail.com>
2009-03-14 10:33:27 -0500

    limit image types to just gif and jpeg

--- app/models/mpd_letter_image.rb
+++ app/models/mpd_letter_image.rb
@@ -1,6 +1,19 @@
 class MpdLetterImage < ActiveRecord::Base
   has_attachment :storage => :s3,
-                 :thumbnails => { :thumb => '50x50!', :print => '400>' }
+                 :thumbnails => { :thumb => '50x50!', :print => '400>' },
+                 :content_type => [
+                                    'image/jpeg',
+                                    'image/pjpeg',
+                                    'image/jpg',
+                                    'image/gif',
+                                    'image/jpg',
+                                    'image/jp_',
+                                    'application/jpg',
+                                    'application/x-jpg',
+                                    'image/vnd.swiftview-jpeg',
+                                    'image/gi_',
+                                    'image/x-citrix-pjpeg'
+                                  ]
 
   belongs_to :mpd_letter
   
