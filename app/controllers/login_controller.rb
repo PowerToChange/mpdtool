f13b126e8bf7c034c433105c28d1405548942572
Nathan Untiedt <Nate.Untiedt@uscm.org>
2010-07-02 09:52:11 -0400

    Changed "signin.mygcx.org" to "signin.ccci.org"

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,7 +3,7 @@ class LoginController < ApplicationController
   layout "login"
 
   def login
-    cas_url = 'https://signin.mygcx.org/cas/login'
+    cas_url = 'https://signin.ccci.org/cas/login'
     @gcx_service_url = cas_url + '?service=' + url_for(:action => :login)
 
     @forgot_password_link = get_forgot_password_link
