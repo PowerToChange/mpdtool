eb66e124baf179e16b677faeb0d407ffd72e88aa
Andrew Roth <andrewroth@gmail.com>
2010-12-16 15:15:04 -0500

    clean up locale stuff

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,2 +1,7 @@
 I18n.default_locale = "en-CA"
-I18n.load_path += Dir.glob("config/locales/*.yml")
+I18n.load_path.sort! do |a,b| 
+  if a =~ /en-CA.rb/ then 1
+  elsif b =~ /en-CA.rb/ then -1
+  else a <=> b
+  end
+end
--- config/locales/en-CA.rb
+++ config/locales/en-CA.rb
@@ -97,6 +97,42 @@
     :help_email => "For help please contact helpdesk@c4c.ca",
     :cheques_payable_to => "Power to Change Ministries",
     :check => "cheque",
-    :activerecord => { :attributes => { :mpd_contact => { :zip => 'Postal Code' } } }
+=begin
+    :activerecord => {
+      :attributes => { :mpd_contact => { :zip => 'Postal Code' } }#,
+=begin
+      :errors => {
+        :messages => {
+          :inclusion => "is not included in the list",
+          :exclusion => "is reserved",
+          :invalid => "is invalid",
+          :confirmation => "doesn't match confirmation",
+          :accepted => "must be accepted",
+          :empty => "can't be empty",
+          :blank => "can't be blank",
+          :too_long => "is too long (maximum is %{count} characters)",
+          :too_short => "is too short (minimum is %{count} characters)",
+          :wrong_length => "is the wrong length (should be %{count} characters)",
+          :taken => "has already been taken",
+          :not_a_number => "is not a number",
+          :greater_than => "must be greater than %{count}",
+          :greater_than_or_equal_to => "must be greater than or equal to %{count}",
+          :equal_to => "must be equal to %{count}",
+          :less_than => "must be less than %{count}",
+          :less_than_or_equal_to => "must be less than or equal to %{count}",
+          :odd => "must be odd",
+          :even => "must be even",
+          :record_invalid => "Validation failed: %{errors}"
+        },
+        :template => {
+          :header => {
+            :one => "1 error prohibited this %{model} from being saved",
+            :other => "%{count} errors prohibited this %{model} from being saved"
+          },
+          :body => "There were problems with the following fields:"
+        }
+      }
+=end
+    #},
   },
 }
