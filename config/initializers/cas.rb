b04d65852db6c122bdf3d1a7ed2d74ebca90e757
Andrew Roth <andrewroth@gmail.com>
2011-10-25 17:48:52 -0400

    switch to the key

--- config/initializers/cas.rb
+++ config/initializers/cas.rb
@@ -5,17 +5,17 @@ require 'casclient/frameworks/rails/filter'
 cas_logger = CASClient::Logger.new(RAILS_ROOT+'/log/cas.log')
 cas_logger.level = Logger::DEBUG
 
-CASClient::Frameworks::Rails::Filter.configure(
-    :cas_base_url  => "https://signin.mygcx.org/cas/",
-    :login_url     => "https://signin.mygcx.org/cas/login",
-    :logout_url    => "https://signin.mygcx.org/cas/logout",
-    :validate_url  => "https://signin.mygcx.org/cas/proxyValidate",    
-    :proxy_retrieval_url => "https://pat.powertochange.org/cas_proxy_callback/retrieve_pgt",
-    :proxy_callback_url => "https://pat.powertochange.org/cas_proxy_callback/receive_pgt",
-    :username_session_key => :cas_user,
-    :extra_attributes_session_key => :cas_extra_attributes,
-    :logger => cas_logger,
-    :authenticate_on_every_request => false
-  )
-
 CAS_LOGIN_SCREEN = true
+
+CASClient::Frameworks::Rails::Filter.configure(
+  :cas_base_url  => "https://thekey.me/cas/",
+  :login_url     => "https://thekey.me/cas/login",
+  :logout_url    => "https://thekey.me/cas/logout",
+  :validate_url  => "https://thekey.me/cas/proxyValidate",    
+  :proxy_retrieval_url => "https://pat.powertochange.org/cas_proxy_callback/retrieve_pgt",
+  :proxy_callback_url => "https://pat.powertochange.org/cas_proxy_callback/receive_pgt",
+  :username_session_key => :cas_user,
+  :extra_attributes_session_key => :cas_extra_attributes,
+  :logger => cas_logger,
+  :authenticate_on_every_request => false
+)
